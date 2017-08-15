95ea8058dbe3a8978685223ca04a174f02c276e3
erreur404 <attiewilly@gmail.com>
2017-08-15 16:28:46 +0200

    plotting an approximation of the current acceleration by 'derivating' the current speed value

--- plotresult.m
+++ plotresult.m
@@ -41,142 +41,157 @@ display_index = 1;
 col = 1;
 t = datalog(:,col);col += 1;
 
+% shifting left of 10 seconds
+t = t-10;
 % limitting the experiment to 60 seconds
+index0 = find(t >= 0);
 index60 = find (t >= 60);
 if (length(index60) == 0)
   index60 = length(t);
 end
 
-t = t(1:index60, 1);
+t = t(index0:index60, 1);
 
-desPosx = datalog(1:index60,col);col += 1;
-desPosy = datalog(1:index60,col);col += 1;
-desPosz = datalog(1:index60,col);col += 1;
-desVelx = datalog(1:index60,col);col += 1;
-desVely = datalog(1:index60,col);col += 1;
-desVelz = datalog(1:index60,col);col += 1;
-desAccx = datalog(1:index60,col);col += 1;
-desAccy = datalog(1:index60,col);col += 1;
-desAccz = datalog(1:index60,col);col += 1;
-curPosx = datalog(1:index60,col);col += 1;
-curPosy = datalog(1:index60,col);col += 1;
-curPosz = datalog(1:index60,col);col += 1;
-curVelx = datalog(1:index60,col);col += 1;
-curVely = datalog(1:index60,col);col += 1;
-curVelz = datalog(1:index60,col);col += 1;
-curAccx = datalog(1:index60,col);col += 1;
-curAccy = datalog(1:index60,col);col += 1;
-curAccz = datalog(1:index60,col);col += 1;
+desPosx = datalog(index0:index60,col);col += 1;
+desPosy = datalog(index0:index60,col);col += 1;
+desPosz = datalog(index0:index60,col);col += 1;
+desVelx = datalog(index0:index60,col);col += 1;
+desVely = datalog(index0:index60,col);col += 1;
+desVelz = datalog(index0:index60,col);col += 1;
+desAccx = datalog(index0:index60,col);col += 1;
+desAccy = datalog(index0:index60,col);col += 1;
+desAccz = datalog(index0:index60,col);col += 1;
+curPosx = datalog(index0:index60,col);col += 1;
+curPosy = datalog(index0:index60,col);col += 1;
+curPosz = datalog(index0:index60,col);col += 1;
+curVelx = datalog(index0:index60,col);col += 1;
+curVely = datalog(index0:index60,col);col += 1;
+curVelz = datalog(index0:index60,col);col += 1;
+curAccx = datalog(index0:index60,col);col += 1;
+curAccy = datalog(index0:index60,col);col += 1;
+curAccz = datalog(index0:index60,col);col += 1;
 if JOINT_OP
-desPosJ1= datalog(1:index60,col);col += 1; % ===== des pos joints
-curPosJ1= datalog(1:index60,col);col += 1; % ===== cur pos vel tor joints
-curPosJ2= datalog(1:index60,col);col += 1;
-curPosJ3= datalog(1:index60,col);col += 1;
-curPosJ4= datalog(1:index60,col);col += 1;
-curPosJ5= datalog(1:index60,col);col += 1;
-curPosJ6= datalog(1:index60,col);col += 1;
-curPosJ7= datalog(1:index60,col);col += 1;
-curVelJ1= datalog(1:index60,col);col += 1;
-curVelJ2= datalog(1:index60,col);col += 1;
-curVelJ3= datalog(1:index60,col);col += 1;
-curVelJ4= datalog(1:index60,col);col += 1;
-curVelJ5= datalog(1:index60,col);col += 1;
-curVelJ6= datalog(1:index60,col);col += 1;
-curVelJ7= datalog(1:index60,col);col += 1;
-curTorJ1= datalog(1:index60,col);col += 1;
-curTorJ2= datalog(1:index60,col);col += 1;
-curTorJ3= datalog(1:index60,col);col += 1;
-curTorJ4= datalog(1:index60,col);col += 1;
-curTorJ5= datalog(1:index60,col);col += 1;
-curTorJ6= datalog(1:index60,col);col += 1;
-curTorJ7= datalog(1:index60,col);col += 1;
-desTorJ1= datalog(1:index60,col);col += 1;
-desTorJ2= datalog(1:index60,col);col += 1;
-desTorJ3= datalog(1:index60,col);col += 1;
-desTorJ4= datalog(1:index60,col);col += 1;
-desTorJ5= datalog(1:index60,col);col += 1;
-desTorJ6= datalog(1:index60,col);col += 1;
-desTorJ7= datalog(1:index60,col);col += 1;
+desPosJ1= datalog(index0:index60,col);col += 1; % ===== des pos joints
+curPosJ1= datalog(index0:index60,col);col += 1; % ===== cur pos vel tor joints
+curPosJ2= datalog(index0:index60,col);col += 1;
+curPosJ3= datalog(index0:index60,col);col += 1;
+curPosJ4= datalog(index0:index60,col);col += 1;
+curPosJ5= datalog(index0:index60,col);col += 1;
+curPosJ6= datalog(index0:index60,col);col += 1;
+curPosJ7= datalog(index0:index60,col);col += 1;
+curVelJ1= datalog(index0:index60,col);col += 1;
+curVelJ2= datalog(index0:index60,col);col += 1;
+curVelJ3= datalog(index0:index60,col);col += 1;
+curVelJ4= datalog(index0:index60,col);col += 1;
+curVelJ5= datalog(index0:index60,col);col += 1;
+curVelJ6= datalog(index0:index60,col);col += 1;
+curVelJ7= datalog(index0:index60,col);col += 1;
 
-out_jointPosLimitInfJ1 = datalog(1:index60,col);col += 1;
-out_jointPosLimitInfJ2 = datalog(1:index60,col);col += 1;
-out_jointPosLimitInfJ3 = datalog(1:index60,col);col += 1;
-out_jointPosLimitInfJ4 = datalog(1:index60,col);col += 1;
-out_jointPosLimitInfJ5 = datalog(1:index60,col);col += 1;
-out_jointPosLimitInfJ6 = datalog(1:index60,col);col += 1;
-out_jointPosLimitInfJ7 = datalog(1:index60,col);col += 1;
+% computing real time acceleration from velocities
+col -= 7; % going back to velocities
+echant = round(length(t)/60);
+curAccJ1= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
+curAccJ2= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
+curAccJ3= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
+curAccJ4= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
+curAccJ5= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
+curAccJ6= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
+curAccJ7= datalog(index0+echant:index60+echant,col)-datalog(index0:index60,col);col += 1;
 
-out_jointPosLimitSupJ1 = datalog(1:index60,col);col += 1;
-out_jointPosLimitSupJ2 = datalog(1:index60,col);col += 1;
-out_jointPosLimitSupJ3 = datalog(1:index60,col);col += 1;
-out_jointPosLimitSupJ4 = datalog(1:index60,col);col += 1;
-out_jointPosLimitSupJ5 = datalog(1:index60,col);col += 1;
-out_jointPosLimitSupJ6 = datalog(1:index60,col);col += 1;
-out_jointPosLimitSupJ7 = datalog(1:index60,col);col += 1;
+curTorJ1= datalog(index0:index60,col);col += 1;
+curTorJ2= datalog(index0:index60,col);col += 1;
+curTorJ3= datalog(index0:index60,col);col += 1;
+curTorJ4= datalog(index0:index60,col);col += 1;
+curTorJ5= datalog(index0:index60,col);col += 1;
+curTorJ6= datalog(index0:index60,col);col += 1;
+curTorJ7= datalog(index0:index60,col);col += 1;
+desTorJ1= datalog(index0:index60,col);col += 1;
+desTorJ2= datalog(index0:index60,col);col += 1;
+desTorJ3= datalog(index0:index60,col);col += 1;
+desTorJ4= datalog(index0:index60,col);col += 1;
+desTorJ5= datalog(index0:index60,col);col += 1;
+desTorJ6= datalog(index0:index60,col);col += 1;
+desTorJ7= datalog(index0:index60,col);col += 1;
 
-out_jointVelLimitInfJ1 = datalog(1:index60,col);col += 1;
-out_jointVelLimitInfJ2 = datalog(1:index60,col);col += 1;
-out_jointVelLimitInfJ3 = datalog(1:index60,col);col += 1;
-out_jointVelLimitInfJ4 = datalog(1:index60,col);col += 1;
-out_jointVelLimitInfJ5 = datalog(1:index60,col);col += 1;
-out_jointVelLimitInfJ6 = datalog(1:index60,col);col += 1;
-out_jointVelLimitInfJ7 = datalog(1:index60,col);col += 1;
+out_jointPosLimitInfJ1 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitInfJ2 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitInfJ3 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitInfJ4 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitInfJ5 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitInfJ6 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitInfJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointVelLimitSupJ1 = datalog(1:index60,col);col += 1;
-out_jointVelLimitSupJ2 = datalog(1:index60,col);col += 1;
-out_jointVelLimitSupJ3 = datalog(1:index60,col);col += 1;
-out_jointVelLimitSupJ4 = datalog(1:index60,col);col += 1;
-out_jointVelLimitSupJ5 = datalog(1:index60,col);col += 1;
-out_jointVelLimitSupJ6 = datalog(1:index60,col);col += 1;
-out_jointVelLimitSupJ7 = datalog(1:index60,col);col += 1;
+out_jointPosLimitSupJ1 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitSupJ2 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitSupJ3 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitSupJ4 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitSupJ5 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitSupJ6 = datalog(index0:index60,col);col += 1;
+out_jointPosLimitSupJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointAccLimitInfJ1 = datalog(1:index60,col);col += 1;
-out_jointAccLimitInfJ2 = datalog(1:index60,col);col += 1;
-out_jointAccLimitInfJ3 = datalog(1:index60,col);col += 1;
-out_jointAccLimitInfJ4 = datalog(1:index60,col);col += 1;
-out_jointAccLimitInfJ5 = datalog(1:index60,col);col += 1;
-out_jointAccLimitInfJ6 = datalog(1:index60,col);col += 1;
-out_jointAccLimitInfJ7 = datalog(1:index60,col);col += 1;
+out_jointVelLimitInfJ1 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitInfJ2 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitInfJ3 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitInfJ4 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitInfJ5 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitInfJ6 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitInfJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointAccLimitSupJ1 = datalog(1:index60,col);col += 1;
-out_jointAccLimitSupJ2 = datalog(1:index60,col);col += 1;
-out_jointAccLimitSupJ3 = datalog(1:index60,col);col += 1;
-out_jointAccLimitSupJ4 = datalog(1:index60,col);col += 1;
-out_jointAccLimitSupJ5 = datalog(1:index60,col);col += 1;
-out_jointAccLimitSupJ6 = datalog(1:index60,col);col += 1;
-out_jointAccLimitSupJ7 = datalog(1:index60,col);col += 1;
+out_jointVelLimitSupJ1 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitSupJ2 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitSupJ3 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitSupJ4 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitSupJ5 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitSupJ6 = datalog(index0:index60,col);col += 1;
+out_jointVelLimitSupJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointAccDynLimitInfJ1 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitInfJ2 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitInfJ3 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitInfJ4 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitInfJ5 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitInfJ6 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitInfJ7 = datalog(1:index60,col);col += 1;
+out_jointAccLimitInfJ1 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitInfJ2 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitInfJ3 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitInfJ4 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitInfJ5 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitInfJ6 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitInfJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointAccDynLimitSupJ1 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitSupJ2 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitSupJ3 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitSupJ4 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitSupJ5 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitSupJ6 = datalog(1:index60,col);col += 1;
-out_jointAccDynLimitSupJ7 = datalog(1:index60,col);col += 1;
+out_jointAccLimitSupJ1 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitSupJ2 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitSupJ3 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitSupJ4 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitSupJ5 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitSupJ6 = datalog(index0:index60,col);col += 1;
+out_jointAccLimitSupJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointTorqueLimitInfJ1 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitInfJ2 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitInfJ3 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitInfJ4 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitInfJ5 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitInfJ6 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitInfJ7 = datalog(1:index60,col);col += 1;
+out_jointAccDynLimitInfJ1 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitInfJ2 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitInfJ3 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitInfJ4 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitInfJ5 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitInfJ6 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitInfJ7 = datalog(index0:index60,col);col += 1;
 
-out_jointTorqueLimitSupJ1 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitSupJ2 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitSupJ3 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitSupJ4 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitSupJ5 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitSupJ6 = datalog(1:index60,col);col += 1;
-out_jointTorqueLimitSupJ7 = datalog(1:index60,col);col += 1;
+out_jointAccDynLimitSupJ1 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitSupJ2 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitSupJ3 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitSupJ4 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitSupJ5 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitSupJ6 = datalog(index0:index60,col);col += 1;
+out_jointAccDynLimitSupJ7 = datalog(index0:index60,col);col += 1;
+
+out_jointTorqueLimitInfJ1 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitInfJ2 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitInfJ3 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitInfJ4 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitInfJ5 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitInfJ6 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitInfJ7 = datalog(index0:index60,col);col += 1;
+
+out_jointTorqueLimitSupJ1 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitSupJ2 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitSupJ3 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitSupJ4 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitSupJ5 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitSupJ6 = datalog(index0:index60,col);col += 1;
+out_jointTorqueLimitSupJ7 = datalog(index0:index60,col);col += 1;
 
 end
 
@@ -356,12 +371,14 @@ for joint=1:display_y
     xlabel("Time [sec]");
     ylabel("torque [N/m]");
     title(sprintf("Joint %d torques", joint));
-    
+
     %sprintf("displaying accel graph") % no accel feedback ?
     subplot(display_y, display_x, display_index);display_index += 1; % 1
-    des = eval(sprintf("out_jointAccLimitInfJ%d",joint)); cur = eval(sprintf("out_jointAccLimitSupJ%d", joint));
-    sup = eval(sprintf("out_jointAccDynLimitInfJ%d", joint)); inf = eval(sprintf("out_jointAccDynLimitSupJ%d", joint));
-    plot(t, des, "-b");
+    infStat = eval(sprintf("out_jointAccLimitInfJ%d",joint)); cur = eval(sprintf("curAccJ%d", joint)); supStat = eval(sprintf("out_jointAccLimitSupJ%d", joint));
+    inf = eval(sprintf("out_jointAccDynLimitInfJ%d", joint)); sup = eval(sprintf("out_jointAccDynLimitSupJ%d", joint));
+    plot(t, infStat, "-b");
+    hold on;
+    plot(t, supStat, "-c");
     hold on;
     plot(t, cur, "-r");
     hold on;
@@ -370,14 +387,14 @@ for joint=1:display_y
     plot(t, sup, "-k");
     hold off;
     if (mean(cur(floor(size(cur)(1)/2):size(cur)(1))) < (max(cur) + min(cur))/2)
-        %legend({"static inf limit", "static sup limit", "dyn inf limit", "dyn sup limit"}, 'Location', 'northeast');
+        %legend({"actual", "static inf limit", "static sup limit", "dyn inf limit", "dyn sup limit"}, 'Location', 'northeast');
     else
-        %legend({"static inf limit", "static sup limit", "dyn inf limit", "dyn sup limit"}, 'Location', 'northeast');
+        %legend({"actual", static inf limit", "static sup limit", "dyn inf limit", "dyn sup limit"}, 'Location', 'northeast');
     endif
     xlabel("Time [sec]");
-    ylabel("torque [N/m]");
+    ylabel("acceleration [m/s²]");
     title(sprintf("Joint %d acceleration", joint));
-    
+
     %sprintf("displaying velocity graph")
     subplot(display_y, display_x, display_index);display_index += 1; % 1
     cur = eval(sprintf("curVelJ%d", joint));
@@ -386,7 +403,7 @@ for joint=1:display_y
     hold on;
     plot(t, inf, "-y");
     hold on;
-    plot(t, sup, "-b");
+    plot(t, sup, "-k");
     hold off;
     if (mean(cur(floor(size(cur)(1)/2):size(cur)(1))) < (max(cur) + min(cur))/2)
         %legend({"actual", "inf limit", "sup limit"}, 'Location', 'northeast');
@@ -396,8 +413,8 @@ for joint=1:display_y
     xlabel("Time [sec]");
     ylabel("speed [rad/s]");
     title(sprintf("Joint %d speeds", joint));
-    
-    
+
+
     %sprintf("displaying position graph")
     subplot(display_y, display_x, display_index);display_index += 1; % 1
     cur = eval(sprintf("curPosJ%d", joint));
@@ -416,7 +433,7 @@ for joint=1:display_y
     xlabel("Time [sec]");
     ylabel("position [rad]");
     title(sprintf("Joint %d positions", joint));
-    
+
     %sprintf("                    for joint %d", joint)
 end
 
